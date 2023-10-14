9c890a959fa342e6faa10870a3864678eacd912b
Will C IV <cleghornw04@proton.me>
2023-10-14 09:57:31 -0600

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -2,27 +2,23 @@
   description = "Development Shell for MEEQ";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs;
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            exa
-            go
-          ];
-
-          shellHook = ''
-            alias ls=exa
-          '';
-        };
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let 
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              go
+            ];
+          };
+        }
+      );
 }
